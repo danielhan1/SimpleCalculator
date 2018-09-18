55ce4a0981f38211a61d2a62644008d94bff575c
Daniel <42978531+danielhan1@users.noreply.github.com>
2018-09-17 20:35:08 -0400

    Add files via upload

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,24 @@
-
-public class Calculator
+/**
+ * 
+ * @author danielhan
+ *
+ */
+public class Calculator 
 {
 
-	public int add(int num1, int num2)
-	{
-		int ans = num1 + num2;
-		return ans;
+		public int add(int num1, int num2)
+		{
+				int ans = num1 + num2;
+				return ans;
 		
-	}
+		}
+		
+		public int sub(int num1, int num2)
+		{
+				int ans = num1 - num2;
+				return ans;
 }
+
+}
+
+
--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -1,10 +1,16 @@
-
+/**
+ * 
+ * @author danielhan
+ *
+ */
 public class CalculatorMain {
 
-	public static void main(String[] args)
-	{
-		Calculator calc = new Calculator();
-		System.out.print(calc.add(2,  4));
-		
-	}
+		public static void main(String[] args)
+		{
+				Calculator calc = new Calculator();
+				System.out.println("The sum of 2 and 4 is " + calc.add(2,  4) + ".");
+				System.out.print("The difference of 2 and 4 is " + calc.sub(2, 4) + ".");
+				
+				
+		}
 }
